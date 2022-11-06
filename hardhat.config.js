6306f9c45f647ef475805c82594fee7e4a27a074
AdeelK-tech <adeeltech12@gmail.com>
2022-11-06 21:05:25 +0500

    deployed contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+ALCHEMY_API_KEY = "7R-Kjv7qdzXJ4wRJSwFWvcfrqCXbzd6Z";
+PRIVATE_KEY =
+  "8dd9a1413df575220c56e07aa6954e21de61129d9ebc5a89816513cc86fa0167";
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const driver = await ethers.getContractFactory("Driver");
+  const driverContract = await driver.deploy();
+  const rider = await ethers.getContractFactory("Rider");
+  const riderContract = await rider.deploy();
+  const rideSharing = await ethers.getContractFactory("RideSharing");
+  const rideSharingCon = await rideSharing.deploy();
+  console.log("driver address:", driverContract.address);
+  console.log("rider address:", riderContract.address);
+  console.log("ridesharing address:", rideSharingCon.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
